7a29c59d3a8bb5a42dde013f7474650ef6abad96
Tema26 <moskvitin.artem@gmail.com>
2022-02-04 10:45:19 +0300

    some chng5

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
 const inputWord = document.querySelector('.input_word')
 const lastLetterInWord = inputWord.value
-const result = document.querySelector('.result')
+const modifiedNoun = document.querySelector('.result')
+let possibleEndingThisCase = ['а']
 
 // Родительный падеж
 const btnGenetive = document.querySelector('.btn_genitive')
-const resultGenitiveCase = btnGenetive.addEventListener('click', () => {
-
-    if (lastLetterInWord.slice(-1) == 'а') {
-        const res = lastLetterInWord.replace(/.$/, 'и')
-        result.innerHTML += res
+btnGenetive.addEventListener('click', () => {
+    if (lastLetterInWord.slice(-1) == possibleEndingThisCase) {
+        const changeNoun = lastLetterInWord.replace(/.$/, 'и')
+        modifiedNoun.insertAdjacentHTML("afterend", changeNoun )
     } 
     
 })
 
 // Дательный падеж
 const btnDative = document.querySelector('.btn_dative')
-const resultDativeCase = btnDative.addEventListener('click', () => {
-    if (lastLetterInWord.slice(-1) == 'а') {
-        const res = lastLetterInWord.replace(/.$/, 'е')
-        result.innerHTML += res
+btnDative.addEventListener('click', () => {
+    if (lastLetterInWord.slice(-1) == possibleEndingThisCase) {
+        const changeNoun = lastLetterInWord.replace(/.$/, 'е')
+        modifiedNoun.insertAdjacentHTML("afterend", changeNoun )
     } 
 
 })
 
 // Винительный падеж
 const btnАccusative = document.querySelector('.btn_accusative')
-const resultAccusativeCase = btnАccusative.addEventListener('click', () => {
-    if (lastLetterInWord.slice(-1) == 'а') {
-        const res = lastLetterInWord.replace(/.$/, 'у')
-        result.innerHTML += res
+btnАccusative.addEventListener('click', () => {
+    if (lastLetterInWord.slice(-1) == possibleEndingThisCase) {
+        const changeNoun = lastLetterInWord.replace(/.$/, 'у')
+        modifiedNoun.insertAdjacentHTML("afterend", changeNoun )
     } 
 
 })
 
 // Творительный падеж
 const btnInstrumental = document.querySelector('.btn_instrumental')
-const resultInstrumentalCase = btnInstrumental.addEventListener('click', () => {
-    if (lastLetterInWord.slice(-1) == 'а') {
-        const res = lastLetterInWord.replace(/.$/, 'ой')
-        result.innerHTML += res
+btnInstrumental.addEventListener('click', () => {
+    if (lastLetterInWord.slice(-1) == possibleEndingThisCase) {
+        const changeNoun = lastLetterInWord.replace(/.$/, 'ой')
+        modifiedNoun.insertAdjacentHTML("afterend", changeNoun )
     } 
 
 })
 
 // Предложный падеж
 const btnPrepositional = document.querySelector('.btn_prepositional')
-const resultPrepositionalCase = btnPrepositional.addEventListener('click', () => {
-    if (lastLetterInWord.slice(-1) == 'а') {
-        const res = lastLetterInWord.replace('', 'о ').replace(/.$/, 'е');
-        result.innerHTML += res
+btnPrepositional.addEventListener('click', () => {
+    if (lastLetterInWord.slice(-1) == possibleEndingThisCase) {
+        const changeNoun = lastLetterInWord.replace('', 'о ').replace(/.$/, 'е');
+        modifiedNoun.insertAdjacentHTML("afterend", changeNoun )
 }
 })
 
